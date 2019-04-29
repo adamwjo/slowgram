20236a850ff09e2298f5c6fc864a59d5bb792c80
adamwjo <adamwjo@gmail.com>
2019-04-29 14:56:05 -0400

    sucessful image uploading without saving to file system

--- src/app.py
+++ src/app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
-import os
 
-APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # once flask has been imported an instance of the flask app must be instantiated
 app = Flask(__name__)
@@ -12,19 +10,11 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    target = os.path.join(APP_ROOT, 'static/')
-    print(target)
-
-    if not os.path.isdir(target):
-        os.mkdir(target)
+    
 
     for upload in request.files.getlist("file"):
         print(upload)
         filename = upload.filename
-        destination = "/".join([target, filename])
-        print(destination)
-        upload.save(destination)
-
     return render_template('show.html', filename=filename)
 
 @app.route('/upload/<filename>')
